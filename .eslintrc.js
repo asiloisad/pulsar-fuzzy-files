d898474cfefbc7a2aa6fefe14cd5eb0a05f2f3db
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-02-05 05:34:38 +0100

    fix lints

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  globals: {
+    atom: "readonly",
+    emit: "readonly",
+    requestAnimationFrame: "readonly",
+  },
+  rules: {
+    "no-cond-assign": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^etch$" }],
+  },
+};
